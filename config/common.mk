48d4e2da65846a1c8fdcf4ed643c291e54c104c8
Mati Halperin <mati@cypheros.co>
2018-03-08 14:18:25 -0300

    vendor: Grab some stuff from Lineage's vendor

--- config/common.mk
+++ config/common.mk
@@ -1,9 +1,145 @@
-# Sensitive Phone Numbers list
-PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
 
-# World APN list
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    keyguard.no_require_sim=true
+
+# Default notification/alarm sounds
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Hassium.ogg
+
+ifeq ($(BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE),)
+  PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.device.cache_dir=/data/cache
+else
+  PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.device.cache_dir=/cache
+endif
+
+# Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/aosp/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/aosp/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/aosp/prebuilt/common/bin/50-lineage.sh:system/addon.d/50-lineage.sh \
+    vendor/aosp/prebuilt/common/bin/blacklist:system/addon.d/blacklist
+
+# Backup Services whitelist
+PRODUCT_COPY_FILES += \
+    vendor/aosp/config/permissions/backup.xml:system/etc/sysconfig/backup.xml
+
+# Signature compatibility validation
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/bin/otasigcheck.sh:install/bin/otasigcheck.sh
+
+# init.d support
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/bin/sysinit:system/bin/sysinit
+
+ifneq ($(TARGET_BUILD_VARIANT),user)
+# userinit support
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
+endif
+
+# Copy all Lineage-specific init rc files
+$(foreach f,$(wildcard vendor/aosp/prebuilt/common/etc/init/*.rc),\
+	$(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
+
+# Copy over added mimetype supported in libcore.net.MimeUtils
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
+
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Enable wireless Xbox 360 controller support
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
+
+# Include explicitly to work around GMS issues
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full \
+    librsjni
+
+# Extra tools in Lineage
+PRODUCT_PACKAGES += \
+    7z \
+    awk \
+    bash \
+    bzip2 \
+    curl \
+    fsck.ntfs \
+    gdbserver \
+    htop \
+    lib7z \
+    libsepol \
+    micro_bench \
+    mke2fs \
+    mkfs.ntfs \
+    mount.ntfs \
+    oprofiled \
+    pigz \
+    powertop \
+    sqlite3 \
+    strace \
+    tune2fs \
+    unrar \
+    unzip \
+    vim \
+    wget \
+    zip
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+# exFAT tools
+PRODUCT_PACKAGES += \
+    fsck.exfat \
+    mkfs.exfat
+
+# Openssh
+PRODUCT_PACKAGES += \
+    scp \
+    sftp \
+    ssh \
+    sshd \
+    sshd_config \
+    ssh-keygen \
+    start-ssh
+
+# rsync
+PRODUCT_PACKAGES += \
+    rsync
+
+# Stagefright FFMPEG plugin
+PRODUCT_PACKAGES += \
+    libffmpeg_extractor \
+    libffmpeg_omx \
+    media_codecs_ffmpeg.xml
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    media.sf.omx-plugin=libffmpeg_omx.so \
+    media.sf.extractor-plugin=libffmpeg_extractor.so
+
+# Media
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    media.recorder.show_manufacturer_and_model=true
+
+# These packages are excluded from user builds
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_PACKAGES += \
+    procmem \
+    procrank
+endif
+
+DEVICE_PACKAGE_OVERLAYS += vendor/aosp/overlay/common
 
 -include vendor/google/config/common.mk
---
+++ config/common_full_phone.mk
@@ -0,0 +1,5 @@
+# Inherit common stuff
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aosp/config/telephony.mk)
